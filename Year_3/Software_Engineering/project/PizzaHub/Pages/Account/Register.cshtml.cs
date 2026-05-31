0138b121e2fc36d6a161912b404ba777dc0fc4e6
Dian Lazarov <dian.lazzarov@gmail.com>
2026-05-31 19:16:47 +0300

    Add files via upload

---
+++ Year_3/Software_Engineering/project/PizzaHub/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaHub.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            // TODO: Add user registration logic here later
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ Year_3/Software_Engineering/project/PizzaHub/Pages/Search.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaHub.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public string SearchQuery { get; set; } = string.Empty;
+
+        public void OnGet(string q)
+        {
+            SearchQuery = q ?? string.Empty;
+        }
+    }
+}
