5422d269b4dbae40af05d9ab81c35e1a86c01995
Dian Lazarov <dian.lazzarov@gmail.com>
2026-07-02 13:53:24 +0300

    Add files via upload

---
+++ Year_3/Course_Project/project/dido-game-hub/src/components/GameGrid.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import apiClient from "../services/api-client";
+import useGames, { Platform } from "../hooks/useGames";
+import { GameCard } from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  if (error) return <Text>{error}</Text>;
+
+  // Handle unfound/empty search results
+  if (!isLoading && data.length === 0) {
+    return (
+      <Text padding="10px" fontSize="lg" fontWeight="semibold">
+        No results found for {gameQuery.searchText || "your search"}
+      </Text>
+    );
+  }
+
+  return (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameGrid;
